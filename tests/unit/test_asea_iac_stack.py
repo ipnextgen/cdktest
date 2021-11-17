67ad6c2f952e7b5cebe09e26677cbdb56861e302
EC2 Default User <ec2-user@ip-172-31-42-8.ca-central-1.compute.internal>
2021-11-17 02:15:30 +0000

    Initial commit

---
+++ tests/unit/test_asea_iac_stack.py
@@ -0,0 +1,19 @@
+# from aws_cdk import (
+#         core,
+#         assertions
+#     )
+
+# from asea_iac.asea_iac_stack import AseaIacStack
+
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in asea_iac/asea_iac_stack.py
+def test_sqs_queue_created():
+#     app = core.App()
+#     stack = AseaIacStack(app, "asea-iac")
+#     template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
+  pass
